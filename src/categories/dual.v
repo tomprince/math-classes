7cefc5a4a82d09d4ae8b61f81c6c805532fd0d7b
Tom Prince <tom.prince@ualberta.net>
2011-11-24 01:16:32 -0500

    hom_functor hacking.

--- src/categories/dual.v
+++ src/categories/dual.v
@@ -2,19 +2,23 @@ Require Import
   Relation_Definitions abstract_algebra theory.categories interfaces.functors.
 
 Section contents.
+  Notation "C '^op'" := C (at level 1, only parsing).
 
   Context `{c: @Category Object A Aeq Aid Acomp}.
 
-  Instance flipA: Arrows Object := flip A.
+  Global Instance flipA: Arrows Object^op := flip A.
 
-  Global Instance: @CatId Object flipA := Aid.
-  Global Instance: @CatComp Object flipA := λ _ _ _, flip (Acomp _ _ _).
-  Global Instance e: ∀ x y, Equiv (flipA x y) := λ x y, Aeq y x.
+  Global Instance: CatId Object^op := Aid.
+  Global Instance: CatComp Object^op := λ _ _ _, flip (Acomp _ _ _).
+  Global Instance e: ∀ x y: Object^op, Equiv (x ⟶ y) | 1000 := λ x y, Aeq y x.
+  Local Existing Instance e.
 
-  Global Instance: ∀ (x y: Object), Setoid (flipA x y).
-  Proof. intros. change (Setoid (A y x)). apply arrow_equiv. Qed.
+  Global Instance: ∀ (x y: Object^op), Equivalence (e x y).
+  Proof. intros. change (Equivalence ((=): Equiv (A y x))). apply _. Qed.
 
-  Instance: ∀ (x y z: Object), Proper ((=) ==> (=) ==> (=)) (@comp Object flipA _ x y z).
+  Global Instance: ∀ (x y: Object^op), Setoid (x ⟶ y) := {}.
+
+  Global Instance: ∀ (x y z: Object^op), Proper ((=) ==> (=) ==> (=)) (comp x y z).
   Proof.
    intros x y z ? ? E ? ? F.
    change (Acomp z y x x1 x0 = Acomp z y x y1 y0).
@@ -22,7 +26,7 @@ Section contents.
    destruct c. rewrite E, F. reflexivity.
   Qed.
 
-  Global Instance cat: @Category Object flipA _ _ _.
+  Instance cat: @Category Object^op flipA _ _ _.
   Proof with auto.
    destruct c.
    constructor; try apply _; auto.
@@ -33,6 +37,7 @@ Section contents.
   Qed.
 
 End contents.
+  Global Hint Cut [ !*; e; e ] : typeclass_instances.
 
 Section functors.
 
@@ -40,7 +45,7 @@ Section functors.
 
   Context {C D} F `{Functor C (H:=Ce) D (H1:=De) F}.
 
-  Definition fmap_op: @Fmap _ flipA _ flipA F := fun v w => @fmap _ _ _ _ F _ w v.
+  Definition fmap_op: @Fmap C flipA D flipA F := fun v w => fmap F (v:=w)(w:=v).
 
   Global Instance: Functor F fmap_op.
   Proof with intuition.
@@ -48,12 +53,11 @@ Section functors.
     pose proof (functor_from F).
     pose proof (functor_to F).
     constructor; repeat intro.
-        apply cat.
-       apply cat.
-      destruct (functor_morphism F b a).
-      constructor...
-     set (preserves_id F a)...
-    apply (@preserves_comp _ _ Ce _ _ _ _ De _ _ F)...
+    - apply cat.
+    - apply cat.
+    - constructor; typeclasses eauto.
+    - set (preserves_id F a)...
+    - set (preserves_comp F (H3:=H3) g f)...
   Qed.
 
 End functors.
--- src/theory/hom_functor.v
+++ src/theory/hom_functor.v
@@ -2,7 +2,9 @@ Require Import
   Relation_Definitions
   abstract_algebra theory.setoids interfaces.functors theory.categories.
 
-Require categories.setoids.
+Require categories.setoids categories.dual.
+Require categories.functors.
+Require Import workaround_tactics.
 
 Section contents.
 
@@ -31,4 +33,91 @@ Section contents.
    apply comp_assoc.
   Qed.
 
+  Context `{!Functor (F:C -> setoids.Object) F'}.
+  Program Definition l : (functors.object homFrom⟶functors.object F) → setoids.T (F x) := λ X, ` (functors.eta X x) cat_id.
+  Program Definition r' : setoids.T (F x) → (homFrom ⇛ F) := λ X a t, (F' _ _ t) X.
+  Next Obligation.
+    constructor; try typeclasses eauto.
+    intros A B ?.
+    simpl in *.
+    apply Functor0; [rewrite H1|]; reflexivity.
+  Qed.
+  Program Instance r: Inverse l (* setoid.T (F x) → Nat homFrom ⇛ F*) := λ X,
+    functors.arrow (functors.object homFrom) (functors.object F) (r' X) _.
+  Next Obligation.
+    intros a b f s t Hst. simpl.
+    fold (fmap (v:=x)(w:=b) F).
+    fold (fmap (v:=x)(w:=a) F).
+    unfold compose.
+    pose (preserves_comp F f s X X (reflexivity _)) as e.
+    rewrite e; clear e.
+    simpl; unfold compose.
+    simpl in *.
+    pose (functor_morphism F a b).
+    apply s0.
+    pose (λ v w, sm_proper (f:=fmap F(v:=v)(w:=w))).
+    apply p; [assumption|reflexivity].
+  Qed.
+
+  Global Instance sm_l: Setoid_Morphism l.
+  Proof.
+    constructor; try typeclasses eauto.
+    repeat intro.
+    apply H1.
+    reflexivity.
+  Qed.
+  Global Instance: Surjective l.
+  Proof.
+    constructor; try apply _.
+    unfold l, inverse, r.
+    intros a ??.
+    rewrite (preserves_id F x a); simpl; [reflexivity|assumption].
+  Qed.
+  Hint Extern 1 (Proper _ (proj1_sig ?x)) => apply (@sm_proper _ _ _ _ _ (proj2_sig x)) : typeclass_instances.
+
+  Global Instance: Injective l.
+    constructor; try apply _.
+    intros [? Na] [? Nb] ? ? u v ?.
+    simpl in *.
+    rewrite <-(right_identity u).
+    rewrite <-(right_identity v).
+    posed_rewrite (Na _ _ u cat_id cat_id (reflexivity _)).
+    posed_rewrite (Nb _ _ v cat_id cat_id (reflexivity _)).
+    simpl.
+    apply (proper_prf (m:=fmap F) u v); auto.
+  Qed.
+  Global Instance: Bijective l := {}.
+
 End contents.
+
+Section contents2.
+
+  Context `{Category C} (x: C).
+
+  Definition homTo (y: C): setoids.Object := setoids.object (y ⟶ x).
+
+  Global Program Instance: Fmap (Arrows0:=dual.flipA) homTo := λ v w X, (◎ X).
+  Next Obligation.
+    constructor; try apply _.
+    intros f g Hfg.
+    apply comp_proper; auto using reflexivity.
+  Qed.
+
+  Global Instance: Functor homTo _ := {}.
+  Proof.
+  - apply dual.cat.
+  - constructor; try apply _.
+    repeat intro. simpl in *.
+    apply comp_proper; auto.
+  - repeat intro.
+    simpl.
+    rewrite H1.
+    apply right_identity.
+  - repeat intro.
+    simpl.
+    unfold compose.
+    rewrite H1.
+    apply (comp_assoc y0).
+  Qed.
+
+End contents2.
